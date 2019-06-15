e7438d9bd5b7521c109b20545b9c0bfc2a641117
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 11:39:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var obj = {};
+  Object.assign(obj, object);
+  delete obj[key];
+  return obj;
+}
